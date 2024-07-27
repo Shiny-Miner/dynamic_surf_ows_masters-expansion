57e9aa4398262055aa8b2fb3d731ef2d0b0bdbb0
Shiny hunter 08 (Supermannn) <101048219+Shiny-Miner@users.noreply.github.com>
2024-07-27 16:06:17 +0530

    Add files via upload

---
+++ graphics/shiny/zigzagoonShiny.h
@@ -0,0 +1,24 @@
+
+//{{BLOCK(zigzagoonShiny)
+
+//======================================================================
+//
+//	zigzagoonShiny, 32x384@4, 
+//	+ palette 16 entries, not compressed
+//	Total size: 32 = 32
+//
+//	Time-stamp: 2024-07-09, 18:36:48
+//	Exported by Cearn's GBA Image Transmogrifier, v0.8.6
+//	( http://www.coranac.com/projects/#grit )
+//
+//======================================================================
+
+#ifndef GRIT_ZIGZAGOONSHINY_H
+#define GRIT_ZIGZAGOONSHINY_H
+
+#define zigzagoonShinyPalLen 32
+extern const unsigned short zigzagoonShinyPal[16];
+
+#endif // GRIT_ZIGZAGOONSHINY_H
+
+//}}BLOCK(zigzagoonShiny)
