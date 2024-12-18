ce9a7fb53ca08ee444d3e77ddc1b42d2e0340cc7
jmynes <mynesjordan@gmail.com>
2024-12-18 16:28:43 -0600

    added additional sleep seconds

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,7 +1,7 @@
 echo "Running clean"
 python3 scripts/clean.py all
-echo "Sleeping 1 seconds"
-sleep 1
+echo "Sleeping 3 seconds"
+sleep 3
 echo "Running make"
 python3 scripts/make.py
 echo "Sleeping 1 seconds"
